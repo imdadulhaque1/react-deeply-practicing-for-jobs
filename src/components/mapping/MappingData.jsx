6c2da69122a66d7a1edc97dde932fc1c42fce83a
imdadulhaque1 <imdadulhaqueimdad820@gmail.com>
2021-04-20 03:23:50 +0600

    Mapping Practicing and before was using for loop

--- src/components/mapping/MappingData.jsx
+++ src/components/mapping/MappingData.jsx
@@ -3,17 +3,19 @@ import Data from "./data.json";
 
 function MappingData () {
           // console.log(Data[0].name);
-          let items = [];
-          for (let i=0; i<Data.length; i++) {
-                    items.push(
-                              <h4>Name: {Data[i].name}</h4>,
-                              <p>University Name: {Data[i].uni_v}</p>
-                    )
-          }
+          // let items = [];
+          // for (let i=0; i<Data.length; i++) {
+          //           items.push(
+          //                     <h4>Name: {Data[i].name}</h4>,
+          //                     <p>University Name: {Data[i].uni_v}</p>
+          //           )
+          // }
           return (
                     <div>
                     <h1>Practicing Mapping using JSON data</h1>
-                    {items}
+                    {
+                              Data.map((item) => <h4>Name: {item.name}, University Name: {item.uni_v}</h4>  )
+                    }
                     </div>
           )
 }
